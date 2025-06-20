ba44b5503369d693fda34a7f59ed9d10f50fc7f6
Infinity323 <paul_in_plano@hotmail.com>
2025-06-20 08:37:53 -0500

    extra link error handling

--- client/src/components/launch-link/LaunchLink.tsx
+++ client/src/components/launch-link/LaunchLink.tsx
@@ -1,6 +1,11 @@
 import { Button } from "@chakra-ui/react";
-import React, { ReactNode } from "react";
-import { usePlaidLink } from "react-plaid-link";
+import React, { ReactNode, useCallback } from "react";
+import {
+  PlaidLinkOnEvent,
+  PlaidLinkOnExit,
+  PlaidLinkOnSuccess,
+  usePlaidLink,
+} from "react-plaid-link";
 import { useUserContext } from "../../context/UserProvider";
 import { useAccounts } from "../../hooks/useAccounts";
 import { updateItem } from "../../services/itemService";
@@ -21,32 +26,70 @@ const LaunchLink: React.FC<LaunchLinkProps> = (props) => {
   const { refetch } = useAccounts();
   const { openModal } = useModal();
 
-  const onSuccess = async (publicToken, metadata) => {
-    if (!itemId) {
-      try {
-        const accessTokenResponse = await postAccessToken(
-          publicToken,
-          metadata
-        );
-        setAccessTokens((prev: AccessToken[]) => [
-          ...prev,
-          accessTokenResponse,
-        ]);
-      } catch {
+  const onSuccess = useCallback<PlaidLinkOnSuccess>(
+    async (publicToken, metadata) => {
+      if (!itemId) {
+        try {
+          const accessTokenResponse = await postAccessToken(
+            publicToken,
+            metadata
+          );
+          setAccessTokens((prev: AccessToken[]) => [
+            ...prev,
+            accessTokenResponse,
+          ]);
+        } catch {
+          openModal("alert", {
+            header: "Error",
+            body: "The same institution cannot be linked more than once. Please update the existing institution link instead.",
+            status: "error",
+          });
+        }
+      } else {
+        await updateItem(itemId, metadata);
+      }
+      refetch();
+    },
+    [itemId, openModal, refetch, setAccessTokens]
+  );
+
+  const onExit = useCallback<PlaidLinkOnExit>(
+    (error, metadata) => {
+      if (error?.error_code === "INVALID_LINK_TOKEN") {
         openModal("alert", {
           header: "Error",
-          body: "The same institution cannot be linked more than once. Please update the existing institution link instead.",
+          body: "Something went wrong. Please reload the page and try again.",
+          status: "error",
+        });
+      } else if (error) {
+        openModal("alert", {
+          header: "Error",
+          body: `An error occurred while linking your account(s): ${error.display_message}`,
+          status: "error",
         });
       }
-    } else {
-      await updateItem(itemId, metadata);
-    }
-    refetch();
-  };
+    },
+    [openModal]
+  );
+
+  const onEvent = useCallback<PlaidLinkOnEvent>(
+    (eventName, metadata) => {
+      console.log(`Plaid Link event: ${eventName}`, metadata);
+      if (eventName === "ERROR")
+        openModal("alert", {
+          header: "Error",
+          body: `An error occurred while linking your account(s): ${metadata.error_message}`,
+          status: "error",
+        });
+    },
+    [openModal]
+  );
 
   const { open, ready } = usePlaidLink({
     token: linkToken,
     onSuccess,
+    onExit,
+    onEvent,
   });
 
   const onClick = () => open();
--- client/src/components/modals/AlertModal.tsx
+++ client/src/components/modals/AlertModal.tsx
@@ -1,5 +1,8 @@
 import {
+  Box,
   Button,
+  HStack,
+  Icon,
   Modal,
   ModalBody,
   ModalCloseButton,
@@ -7,13 +10,22 @@ import {
   ModalFooter,
   ModalHeader,
   ModalOverlay,
+  Stack,
+  Text,
 } from "@chakra-ui/react";
+import {
+  FaCircleCheck,
+  FaCircleExclamation,
+  FaCircleInfo,
+  FaTriangleExclamation,
+} from "react-icons/fa6";
 import { useModal } from "../../context/GlobalModalProvider";
 import { ModalProps } from "../../types/modalProps";
 
 export interface AlertModalProps {
   header: string;
   body: string;
+  status?: "error" | "info" | "success" | "warning";
 }
 
 const AlertModal: React.FC<ModalProps> = (props) => {
@@ -27,14 +39,45 @@ const AlertModal: React.FC<ModalProps> = (props) => {
     <Modal isOpen={isOpen} onClose={onClose} isCentered>
       <ModalOverlay />
       <ModalContent>
-        <ModalHeader>{modalProps?.header}</ModalHeader>
-        <ModalCloseButton />
-        <ModalBody>{modalProps?.body}</ModalBody>
-        <ModalFooter>
-          <Button mr={3} onClick={onClose}>
-            Close
-          </Button>
-        </ModalFooter>
+        <Stack direction="row" spacing={0}>
+          <ModalHeader>
+            {modalProps?.status && (
+              <Icon
+                as={
+                  modalProps.status === "error"
+                    ? FaCircleExclamation
+                    : modalProps.status === "warning"
+                    ? FaTriangleExclamation
+                    : modalProps.status === "success"
+                    ? FaCircleCheck
+                    : FaCircleInfo
+                }
+                color={
+                  modalProps.status === "error"
+                    ? "red"
+                    : modalProps.status === "warning"
+                    ? "orange"
+                    : modalProps.status === "success"
+                    ? "green"
+                    : "blue"
+                }
+                boxSize={10}
+              />
+            )}
+          </ModalHeader>
+          <Box>
+            <ModalHeader>
+              <Text>{modalProps?.header}</Text>
+            </ModalHeader>
+            <ModalCloseButton />
+            <ModalBody>{modalProps?.body}</ModalBody>
+            <ModalFooter>
+              <Button mr={3} onClick={onClose}>
+                Close
+              </Button>
+            </ModalFooter>
+          </Box>
+        </Stack>
       </ModalContent>
     </Modal>
   );
