3af0d7fde413b5983be74bc8b0e6c6c32f088924
Paul Lee <paul_in_plano@hotmail.com>
2025-06-25 13:41:55 -0500

    show loading spinner during authentication

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,11 @@
-import { ChakraProvider, Divider } from "@chakra-ui/react";
+import {
+  Center,
+  ChakraProvider,
+  Divider,
+  Spinner,
+  Text,
+  VStack,
+} from "@chakra-ui/react";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Footer from "./components/Footer";
@@ -15,6 +22,7 @@ import Overview from "./pages/Overview";
 import Swagger from "./pages/Swagger";
 import Transactions from "./pages/Transactions";
 import { GlobalModalProvider } from "./context/GlobalModalProvider";
+import { useAuth } from "react-oidc-context";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -23,33 +31,47 @@ const queryClient = new QueryClient({
 });
 
 function App() {
+  const { isLoading } = useAuth();
   return (
     <ChakraProvider theme={useTheme()}>
       <QueryClientProvider client={queryClient}>
         <GlobalModalProvider>
           <UserProvider>
-            <BrowserRouter>
-              <Navbar />
-              <Divider />
-              <main className="app-main">
-                <Routes>
-                  <Route path="/" Component={Home} />
-                  <Route path="/overview" Component={Overview} />
-                  <Route path="/insights" Component={Insights} />
-                  <Route path="/budgets" Component={Budgets} />
-                  <Route path="/transactions" Component={Transactions} />
-                  <Route path="/accounts" Component={Accounts} />
-                  <Route path="/login" Component={Login} />
-                  <Route path="/api" Component={Swagger} />
-                  <Route
-                    path="/account-management"
-                    Component={AccountManagement}
-                  />
-                </Routes>
-              </main>
-              <Divider />
-              <Footer />
-            </BrowserRouter>
+            {isLoading ? (
+              <div style={{ height: "100vh", width: "100vw" }}>
+                <Center height="100%" width="100%">
+                  <VStack>
+                    <Spinner color="teal" size="xl" />
+                    <Text fontSize="lg" fontWeight="semibold">
+                      Loading...
+                    </Text>
+                  </VStack>
+                </Center>
+              </div>
+            ) : (
+              <BrowserRouter>
+                <Navbar />
+                <Divider />
+                <main className="app-main">
+                  <Routes>
+                    <Route path="/" Component={Home} />
+                    <Route path="/overview" Component={Overview} />
+                    <Route path="/insights" Component={Insights} />
+                    <Route path="/budgets" Component={Budgets} />
+                    <Route path="/transactions" Component={Transactions} />
+                    <Route path="/accounts" Component={Accounts} />
+                    <Route path="/login" Component={Login} />
+                    <Route path="/api" Component={Swagger} />
+                    <Route
+                      path="/account-management"
+                      Component={AccountManagement}
+                    />
+                  </Routes>
+                </main>
+                <Divider />
+                <Footer />
+              </BrowserRouter>
+            )}
           </UserProvider>
         </GlobalModalProvider>
       </QueryClientProvider>
